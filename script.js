d7852ad11f083e739199f5baeae31e96e250ed33
Jarred Herrington <herringtonjc@yahoo.com>
2018-04-06 23:58:43 -0500

    Add functionality to button, which allows the addition of borders to gridItems

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const gridContainer = document.getElementById("grid-container");
 const resetBtn = document.getElementById("reset");
 const resizeBtn = document.getElementById("resize");
+const borderBtn = document.getElementById("border");
 
 let gridNumber;
 let gridSize = 16;
+let gridItemBorders = false;
 
 resetBtn.addEventListener("click", resetGrid);
 resizeBtn.addEventListener("click", resizeGrid);
+borderBtn.addEventListener("click", addBorder);
 
 function createGrid(gridSize) {
     gridNumber = "auto ".repeat(gridSize);
     gridContainer.style.gridTemplateRows = `${gridNumber}`;
     gridContainer.style.gridTemplateColumns = `${gridNumber}`;
 
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridContainer.appendChild(gridItem);
-        gridItem.addEventListener("mouseover", colorItem);
+    if (!gridItemBorders) {
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridContainer.appendChild(gridItem);
+            gridItem.addEventListener("mouseover", colorItem);
+        }
+    } else {
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridContainer.appendChild(gridItem);
+            gridItem.addEventListener("mouseover", colorItem);
+            gridItem.style.border = "0.5px solid #000";
+        }
     }
 }
 
@@ -39,6 +52,20 @@ function resizeGrid() {
     }
 
     gridSize = prompt("Please enter a new size for the grid");
+    createGrid(gridSize);
+}
+
+function addBorder() {
+    if (!gridItemBorders) {
+        gridItemBorders = true;
+    } else {
+        gridItemBorders = false;
+    }
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
     createGrid(gridSize);
 }
 
