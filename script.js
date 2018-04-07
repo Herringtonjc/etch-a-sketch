511ffdb29544de884b9f6b5c011074177b48dfe6
Jarred Herrington <herringtonjc@yahoo.com>
2018-04-06 22:41:16 -0500

    Add functionality to reset grid/create a new grid

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const gridContainer = document.getElementById("grid-container");
+const resetBtn = document.getElementById("reset");
 
 let gridNumber;
 let gridSize = 16;
+let userGridSize;
+
+resetBtn.addEventListener("click", resetGrid);
 
 function createGrid(gridSize) {
     gridNumber = "auto ".repeat(gridSize);
@@ -18,6 +22,15 @@ function createGrid(gridSize) {
 
 function colorItem(event) {
     event.target.style.backgroundColor = "#000";
+}
+
+function resetGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    userGridSize = prompt("Please enter a new size for the grid");
+    createGrid(userGridSize);
 }
 
 createGrid(gridSize);
